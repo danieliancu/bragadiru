bc5fbaa99ad6f21054a12fc66872063be7d41a11
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-27 18:00:03 +0000

    Create find.php

---
+++ find.php
@@ -0,0 +1,199 @@
+<?php include 'head.php'; ?>
+
+    <script>
+        $(document).ready(function(){
+            
+           $("body").css("background-color","grey"); 
+        });
+    </script>
+	<div class="find">
+		<p>Rezultate oferte din zona Nord</p>
+        <div class="rezultate container-fluid">
+        
+        
+        
+        
+        	<div class="row">
+        		<div class="left col-md-5">
+                	<div class="arrow leftArrow">
+                    	<i class='fas fa-chevron-left'></i>
+                    </div>
+                	<div class="arrow rightArrow">
+                    	<i class='fas fa-chevron-right'></i>
+                    </div>                    
+                	<div class="tip">Vanzare</div>
+                    <div class="pret">De la 100.000$</div>
+					<img src="https://edge.media.datahc.com/HI558163091.jpg">
+            	</div>
+        		<div class="middle col-md-2">
+					 <img src="https://www.taylorwimpey.co.uk/-/media/Head%20Office/Buying%20with%20us/Our%20Homes/OurHomesBanner.png?w=100%25&la=en&hash=349E4FBD616BEA77A43DF61BE421398165FF566C">
+					 <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/197k/196853/88340495/196853_5_Plot_65_IMG_01_0000_max_476x317.jpg">
+					 <img src="https://www.taylorwimpey.co.uk/-/media/Regions/Exeter/Sales/Cranbrook/Phase%203/Images/TW%20Exeter%20-%20Cranbrook%20-%20Show%20homes.jpg">
+            	</div>
+                
+        		<div class="right col-md-5">
+                	<a href="">
+						<h6>
+                    	Apartament confortabil cu 6 camere si dependinte
+                    	<br>
+                    	<span style="color:black;font-weight:300;">Cartierul Calarasi-Nord</span>
+                    	</h6>
+                    </a>
+                    <p>apartament cu predare rapida, acces prin bulevardul Uverturii la doar 15 minute Lujerului legatura directa cu STB 434 pana in fata ansamblului. Apartamentul se preda la cheie, cu finisaje incluse. Se accepta credit ipotecar sau prima casa! COMISION 0%, Tva inclus in pret!</p>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="allSmall">
+                    	<p class="small"><i class='far fa-envelope'></i></i> <a href="">email agent</a></p>
+                     	<p class="small"><i class='fas fa-phone'></i> <a href="">telefon agent</a></p>
+                        <p class="small">Adaugat in 16.04</p>
+                     </div>
+            	</div>
+                
+            </div>
+            
+            
+            
+                      
+            
+            
+<div class="row">
+        		<div class="left col-md-5">
+                	<div class="arrow leftArrow">
+                    	<i class='fas fa-chevron-left'></i>
+                    </div>
+                	<div class="arrow rightArrow">
+                    	<i class='fas fa-chevron-right'></i>
+                    </div>                    
+                	<div class="tip">Vanzare</div>
+                    <div class="pret">De la 100.000$</div>
+					<img src="https://www.millwooddesignerhomes.co.uk/wp-content/uploads/2019/11/Plot.-68-Night-Pond.jpg">
+            	</div>
+        		<div class="middle col-md-2">
+					 <img src="https://www.millwooddesignerhomes.co.uk/wp-content/uploads/2019/11/Neptune-Show-Home-Night.jpg">
+					 <img src="http://3.bp.blogspot.com/-oCsQnCiVrhg/VO5YXAd8_bI/AAAAAAAASAA/XrTNJzzXs8c/s640/Classic-Garden-Style.jpg">
+					 <img src="http://homescorner.com/wp-content/uploads/2014/06/Unique-Crop-Circle-English-Garden-Design-And-Style-Photo.jpg">
+            	</div>
+                
+        		<div class="right col-md-5">
+                	<a href="">
+						<h6>
+                    	Apartament confortabil cu 6 camere si dependinte
+                    	<br>
+                    	<span style="color:black;font-weight:300;">Cartierul Calarasi-Nord</span>
+                    	</h6>
+                    </a>
+                    <p>apartament cu predare rapida, acces prin bulevardul Uverturii la doar 15 minute Lujerului legatura directa cu STB 434 pana in fata ansamblului. Apartamentul se preda la cheie, cu finisaje incluse. Se accepta credit ipotecar sau prima casa! COMISION 0%, Tva inclus in pret!</p>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="allSmall">
+                    	<p class="small"><i class='far fa-envelope'></i></i> <a href="">email agent</a></p>
+                     	<p class="small"><i class='fas fa-phone'></i> <a href="">telefon agent</a></p>
+                        <p class="small">Adaugat in 16.04</p>
+                     </div>
+            	</div>
+                
+            </div> 
+            
+            
+<div class="row">
+        		<div class="left col-md-5">
+                	<div class="arrow leftArrow">
+                    	<i class='fas fa-chevron-left'></i>
+                    </div>
+                	<div class="arrow rightArrow">
+                    	<i class='fas fa-chevron-right'></i>
+                    </div>                    
+                	<div class="tip">Vanzare</div>
+                    <div class="pret">De la 100.000$</div>
+					<img src="http://2.bp.blogspot.com/-C0yHa5pWkL4/UNHmZD3N1UI/AAAAAAAAdQE/Pbb0_EdMXRE/s1600/Modern+homes+beautiful+garden+designs+ideas..jpg">
+            	</div>
+        		<div class="middle col-md-2">
+					 <img src="https://i.pinimg.com/originals/c3/cb/82/c3cb8275d55645d58d864b802ae00baa.jpg">
+					 <img src="https://www.telfordhomes.london/app/uploads/sites/7/2019/09/vb1539543_www.andrewhendry.com21.jpg">
+					 <img src="https://haus-arch.com/wp/wp-content/uploads/2019/03/06_18_hotm.downstairs-1024x576.jpg">
+            	</div>
+                
+        		<div class="right col-md-5">
+                	<a href="">
+						<h6>
+                    	Apartament confortabil cu 6 camere si dependinte
+                    	<br>
+                    	<span style="color:black;font-weight:300;">Cartierul Calarasi-Nord</span>
+                    	</h6>
+                    </a>
+                    <p>apartament cu predare rapida, acces prin bulevardul Uverturii la doar 15 minute Lujerului legatura directa cu STB 434 pana in fata ansamblului. Apartamentul se preda la cheie, cu finisaje incluse. Se accepta credit ipotecar sau prima casa! COMISION 0%, Tva inclus in pret!</p>
+                    <br>
+                    <br>
+                    <br>
+                    <div class="allSmall">
+                    	<p class="small"><i class='far fa-envelope'></i></i> <a href="">email agent</a></p>
+                     	<p class="small"><i class='fas fa-phone'></i> <a href="">telefon agent</a></p>
+                        <p class="small">Adaugat in 16.04</p>
+                     </div>
+            	</div>
+                
+            </div>             
+            
+            
+            
+        	
+        </div>
+        
+    </div>
+
+
+
+
+
+
+
+
+    <div class="footer container-fluid">
+        <div class="row">
+            <div class="col-xl-3">
+                <h2 style="font-weight:600;">Bragadiru<span style="color:var(--color);"><span style="letter-spacing:-5px;"> </span>Homes</span></h2>
+                <a href="">office@bgrd.ro</a>
+                <br>
+                <a href="">+021 45787 958</a>
+                <br>
+                <a href="">Șoseaua Chitilei nr. 45</a>
+                <br>
+                <a href="https://www.facebook.com"><i class='fab fa-facebook-f social'></i></a>
+                <a href="https://www.instagram.com"><i class='fab fa-instagram social'></i></a>
+                <a href="https://www.twitter.com"><i class='fab fa-twitter social'></i></a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Servicii</h4>
+                <a href="">Credit</a>
+                <br>
+                <a href="">Prima casă</a>
+                <br>
+                <a href="">Financiar</a>
+                <br>
+                <a href="">Fișă online</a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Utile</h4>
+                <a href="">Despre noi</a>
+                <br>
+                <a href="">Legal</a>
+                <br>
+                <a href="">Contact</a>
+                <br>
+                <a href="">Resurse</a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Newsletter</h4>
+                <input value="Introdu adresa de email">
+                <br>
+                <button class="btn btn-warning">Trimite</button>
+                <br>
+                <p>Aici poți găsi cele mai noi informații și cele mai noi subiecte dspre ultimele evenimente imobiliare.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
