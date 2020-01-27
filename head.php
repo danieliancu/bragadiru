db166c12692b0215166fc1e3d7e03840ca1add67
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-27 17:59:26 +0000

    Create head.php

---
+++ head.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <title>Bragadiru Real Estate</title>
+    <link rel="stylesheet" href="css/css.css">
+    <link rel="stylesheet" href="css/find.css">
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    
+
+
+
+</head>
+
+<body>
+    <script src="js/js.js"></script>
+
+    <div class="banda">
+        <div><span style="color:var(--color);">Nu găsești pe website?</span> Lasă-ne un mesaj cu ce cauți și datele tale de contact</div>
+        <div>
+            <a href=""><i class='far fa-envelope'></i> info@bragadiru.ro</a>
+            <a href=""><i class='fas fa-phone'></i> 012 345 678</a>
+            <a href="https://www.facebook.com"><i class='fab fa-facebook-f'></i></a>
+            <a href="https://www.instagram.com"><i class='fab fa-instagram'></i></a>
+            <a href="https://www.twitter.com"><i class='fab fa-twitter'></i></a>
+        </div>
+    </div>
+
+        <div class="header">
+            <div class="logo">
+                Bragadiru<span style="color:orange;">Homes</span>
+            </div>
+            <div class="menu">
+                <a href="">Home</a>
+                <a href="">Terenuri</a>
+                <a href="">Case</a>
+                <a href="">Apartamente</a>
+                <a href="">Creditare</a>
+                <a href=""><i class='fas fa-user-alt'></i> Contul tău</a>
+            </div>
+        </div>
+        
+    	<div class="responsiveClose">
+			<i class='fas fa-times'></i>
+        </div>     
+
+        <div class="responsive">
+        	<i class='fas fa-bars'></i>
+        </div>
