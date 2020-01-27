5bb58668b4328bc41ca0ee78eca32e087141d266
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-27 17:58:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,291 @@
+   <?php include 'head.php'; ?>
+        
+        
+    <div class="main">
+        <div class="butoane">
+            <button class="more">Adaugă ofertă</button>
+            <button class="more">Adaugă cerere</button>
+        </div>
+
+        <div class="start">
+            <h1>Soluții complete imobiliare!</h1>
+            <h5>Consultanță imobiliară profesionistă, individuală și dedicată</h5>
+
+            <form class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <label>Tranzacție</label>
+                        <br>
+                        <select>
+                            <option>Vânzare</option>
+                            <option>Cumpărare</option>
+                            <option>Închiriere</option>
+                        </select>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <label>Tip</label>
+                        <br>
+                        <select>
+                            <option>Apartament</option>
+                            <option>Garsonieră</option>
+                            <option>Casă sau vilă</option>     <option>Casă de vacanță</option>
+                            <option>Pensiune</option>         <option>Spațiu comercial</option> <option>Teren</option>
+                        </select>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <label>Preț</label>
+                        <br>
+                        <select>
+                            <option>0-10.000 &#8364;</option>
+                            <option>10.000-20.000 &#8364;</option>
+                            <option>20.000-30.000 &#8364;</option>
+                            <option>30.000-40.000 &#8364;</option>
+                            <option>40.000-50.000 &#8364;</option>
+                            <option>50.000-60.000 &#8364;</option>
+                            <option>60.000-70.000 &#8364;</option>
+                            <option>70.000-80.000 &#8364;</option>
+                            <option>80.000-90.000 &#8364;</option>
+                            <option>90.000-100.000 &#8364;</option>
+                            <option>100.000-150.000 &#8364;</option>
+                            <option>> 150.000 &#8364;</option>
+
+                            
+                            
+                            
+                            
+                            
+                            
+                        </select>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <label>Camere</label>
+                        <br>
+                        <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                        </select>
+                    </div>
+
+                    <div class="col-lg-2">
+                        <label>Zona</label>
+                        <br>
+                        <input type="text">
+                    </div>
+
+                    <div class="col-lg-2">
+                        <button type="submit" onclick="find.html"><i class='fas fa-search'></i> Căutare</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        <img src="https://hottinger.co.uk/wp-content/uploads/2016/07/Hottinger-Home-Background.jpg">
+    </div>
+
+
+    <div class="oferte">
+        <h1>Cele mai noi oferte</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/3067325/original/House/photoshop-any-background-onto-your-photo.jpg">
+                        </div>
+                        <div class="tip">Vânzare</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">De la 100.000 &#8364;</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://kgplastering18.co.uk/wp-content/uploads/elementor/thumbs/home-background-image-odxx1y301m24a5m7aj5nmcey2y30xh9r0s3u7oi9wg.jpg">
+                        </div>
+                        <div class="tip" style="background-color:orange;">Închiriere</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">600&#8364;/lună</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://images.wallpaperscraft.com/image/building_house_cottage_style_design_49252_1920x1080.jpg">
+                        </div>
+                        <div class="tip" style="background-color:orange;">Închiriere</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">600&#8364;/lună</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://newdawnvillas.in/wp-content/uploads/2019/02/New-dawn-villas-background-Home-Page-BG-new.jpg">
+                        </div>
+                        <div class="tip">Vânzare</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">De la 100.000 &#8364;</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://images.wallpaperscraft.com/image/house_residential_design_beautifully_13642_1920x1080.jpg">
+                        </div>
+                        <div class="tip">Vânzare</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">De la 100.000 &#8364;</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oferta">
+                        <div class="imagine">
+                            <img src="https://images.unsplash.com/photo-1523745663588-1bfa973c9b35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80">
+                        </div>
+                        <div class="tip">Vânzare</div>
+                        <div class="info">
+                            <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                            <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                            <p class="pret">De la 100.000 &#8364;</p>
+                            <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a href="" class="more">Mai multe proprietăți</a>
+        </div>
+    </div>
+
+
+
+    <div class="special">
+        <div class="ofertaSpecial">
+            <div class="tip tipSpecial">Ofertă specială</div>
+
+            <div class="info infoSpecial">
+                <h5><a href="">Apartament confortabil 6 camere</a></h5>
+                <h6><i class='fas fa-check'></i> Anunț verificat</h6>
+                <hr>
+                            <div class="allSmall">
+                            	<p class="small"><i class='fas fa-home'></i> 100 m2</p>
+                            	<p class="small"><i class='fas fa-bed'></i> 2 paturi</p>
+                            	<p class="small"><i class='fas fa-bath'></i> 2 băi</p>
+                            </div>
+                <p>Ofertă senațională, la un preț imbatabil. Casa este construita cu rost de dilatare ! Nu au perete despartitor comun , fiecare casa este independenta + polistiren intre case! Dispun poze pe tot parcursul constructiei pentru confirmarea materialelor folosite!</p>
+                <p class="pret">De la 100.000 &#8364;</p>
+                <a href="" id="afla">Află detalii</a>
+            </div>
+        </div>
+        <img src="https://images.wallpaperscraft.com/image/interior_design_style_design_home_villa_living_room_68248_1920x1080.jpg">
+    </div>
+
+
+
+    <div class="footer container-fluid">
+        <div class="row">
+            <div class="col-xl-3">
+                <h2 style="font-weight:600;">Bragadiru<span style="color:var(--color);"><span style="letter-spacing:-5px;"> </span>Homes</span></h2>
+                <a href="">office@bgrd.ro</a>
+                <br>
+                <a href="">+021 45787 958</a>
+                <br>
+                <a href="">Șoseaua Chitilei nr. 45</a>
+                <br>
+                <a href="https://www.facebook.com"><i class='fab fa-facebook-f social'></i></a>
+                <a href="https://www.instagram.com"><i class='fab fa-instagram social'></i></a>
+                <a href="https://www.twitter.com"><i class='fab fa-twitter social'></i></a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Servicii</h4>
+                <a href="">Credit</a>
+                <br>
+                <a href="">Prima casă</a>
+                <br>
+                <a href="">Financiar</a>
+                <br>
+                <a href="">Fișă online</a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Utile</h4>
+                <a href="">Despre noi</a>
+                <br>
+                <a href="">Legal</a>
+                <br>
+                <a href="">Contact</a>
+                <br>
+                <a href="">Resurse</a>
+            </div>
+            <div class="col-xl-3">
+                <h4>Newsletter</h4>
+                <input value="Introdu adresa de email">
+                <br>
+                <button class="btn btn-warning">Trimite</button>
+                <br>
+                <p>Aici poți găsi cele mai noi informații și cele mai noi subiecte dspre ultimele evenimente imobiliare.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
 
